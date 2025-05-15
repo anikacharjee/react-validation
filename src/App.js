a8f1a01e0c8ff8b57397d7191c74ea8707dff12b
Anik Acharjee <anikacharjee@users.noreply.github.com>
2025-05-15 09:38:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NameForm from './components/NameForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <NameForm/>
+    </div>
+  )
+};
+
+export default App;
